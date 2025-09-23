9985ca6dcd89e8b7bf0e859491fe9fadf8c2e749
Li Peng <kailous@live.cn>
2025-09-23 21:56:29 +0800

    chore: update pages artifact action

--- next.config.js
+++ next.config.js
@@ -1,15 +1,28 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
-//
-// module.exports = nextConfig
 const path = require('path');
 
 module.exports = {
-  // 其他配置项...
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.notion.so',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.imgur.com',
+      },
+    ],
+  },
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname);
     return config;
-  }
-}
+  },
+};
